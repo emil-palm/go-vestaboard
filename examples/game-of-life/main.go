5e96fe87dcefdb30a42dfbed8049a0f0795c38b3
Emil Palm <emil@x86.nu>
2023-10-29 15:03:24 +0100

    Updated example of game-of-life

--- examples/game-of-life/main.go
+++ examples/game-of-life/main.go
@@ -20,8 +20,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/mikehelmick/go-vestaboard"
 	"github.com/mikehelmick/go-vestaboard/internal/config"
+	client "github.com/mikehelmick/go-vestaboard/v2/clients/installables"
+	"github.com/mikehelmick/go-vestaboard/v2/layout"
 )
 
 // Conway's game of life!
@@ -34,7 +35,7 @@ func main() {
 		log.Fatalf("error loading config: %v", err)
 	}
 
-	client := vestaboard.New(c.APIKey, c.Secret)
+	client := client.New(c.APIKey, c.Secret)
 
 	subs, err := client.Subscriptions(ctx)
 	if err != nil {
@@ -42,29 +43,29 @@ func main() {
 	}
 	log.Printf("result: %+v", subs)
 
-	l := vestaboard.NewLayout()
-	l.SetColor(1, 2, vestaboard.PoppyRed)
-	l.SetColor(2, 4, vestaboard.PoppyRed)
-	l.SetColor(3, 1, vestaboard.PoppyRed)
-	l.SetColor(3, 2, vestaboard.PoppyRed)
-	l.SetColor(3, 5, vestaboard.PoppyRed)
-	l.SetColor(3, 6, vestaboard.PoppyRed)
-	l.SetColor(3, 7, vestaboard.PoppyRed)
+	l := layout.NewLayout()
+	l.SetColor(1, 2, layout.PoppyRed)
+	l.SetColor(2, 4, layout.PoppyRed)
+	l.SetColor(3, 1, layout.PoppyRed)
+	l.SetColor(3, 2, layout.PoppyRed)
+	l.SetColor(3, 5, layout.PoppyRed)
+	l.SetColor(3, 6, layout.PoppyRed)
+	l.SetColor(3, 7, layout.PoppyRed)
 
 	for i := 0; i < 30; i++ {
-		_, err := client.SendMessage(ctx, subs.Subscriptions[0].ID, l)
+		_, err := client.SendMessage(ctx, subs[0], l)
 		if err != nil {
 			log.Fatalf("error sending message: %v", err)
 		}
 
-		l = nextGeneration(l, vestaboard.PoppyRed)
+		l = nextGeneration(l, layout.PoppyRed)
 
 		time.Sleep(15 * time.Second)
 	}
 }
 
-func nextGeneration(l vestaboard.Layout, c vestaboard.Color) vestaboard.Layout {
-	n := vestaboard.NewLayout()
+func nextGeneration(l layout.Layout, c layout.Color) layout.Layout {
+	n := layout.NewLayout()
 	for x := 0; x < 6; x++ {
 		for y := 0; y < 22; y++ {
 			curAlive := l[x][y] > 0
@@ -95,7 +96,7 @@ var offsets = [][]int{
 	{1, 1},
 }
 
-func aliveNeighbors(l vestaboard.Layout, x, y int) int {
+func aliveNeighbors(l layout.Layout, x, y int) int {
 	count := 0
 	for _, off := range offsets {
 		nx := x + off[0]
